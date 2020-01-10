29664b6763647ecc733969c0cbf2a76723a7b792
rinzin <rt@meditateinfortcollins.org>
2020-01-10 00:26:00 -0700

    Fixed logic errors in TemplatingController for attendance.

--- src/controllers/TemplatingController.php
+++ src/controllers/TemplatingController.php
@@ -26,6 +26,7 @@ class TemplatingController extends Controller
 
 		foreach($allTheQueryParams as $name => $value) {
             //if ($name != 'CRAFT_CSRF_TOKEN') $request .= "[$name]=>$value ";
+            $out .= "[$name]=>$value, ";
             $first3 = substr($name,0,3);
             $remain = substr($name,3);
             if ($first3 == "16+" or $first3 == "<16") {
@@ -33,17 +34,18 @@ class TemplatingController extends Controller
                 $userArray = json_decode($occurrence->userJson, true);
                 if (null == $userArray) {
                     $userArray = [];
+                }
+                if ( array_key_exists($first3, $userArray) && $userArray[$first3] == $value) {
+                    // Skip if no change
                 } else {
-                    if ( array_key_exists($first3, $userArray) && $userArray[$first3] != $value) {
-                        $change++; 
-                        $userArray[$first3] = $value;
-                        $occurrence->userJson = json_encode($userArray);
-                        if ($occurrence->save()) {
-                            $success_chg++;
-                        } else {
-                            $out .= "<p>Error in $name</p><pre>"
-                                 .print_r($occurrence->getErrors(),true)."</pre>";
-                        }
+                    $change++; 
+                    $userArray[$first3] = $value;
+                    $occurrence->userJson = json_encode($userArray);
+                    if ($occurrence->save()) {
+                        $success_chg++;
+                    } else {
+                        $out .= "<p>Error in $name</p><pre>"
+                             .print_r($occurrence->getErrors(),true)."</pre>";
                     }
                 }
             }
